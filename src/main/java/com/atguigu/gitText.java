e158b3c515077aa088af324074c9ef3563114a9a
kuang <1196857190@qq.com>
2021-10-29 20:18:17 +0800

    push commit

--- src/main/java/com/atguigu/gitText.java
+++ src/main/java/com/atguigu/gitText.java
@@ -5,5 +5,6 @@ public class gitText {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git3");
+        System.out.println("push git");
     }
 }
